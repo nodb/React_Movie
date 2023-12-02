bc8c53f1811cd266438ee547b5d59bf4ba56625d
nodb <scnoh02@naver.com>
2023-12-02 21:22:12 +0900

    update: 절대주소 설정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { createBrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
-const App = createBrowserRouter([
-  {
-    path: `${process.env.PUBLIC_URL}/`,
-    element: <Home />,
-  },
-  {
-    path: `${process.env.PUBLIC_URL}/movie/:id`,
-    element: <Detail />,
-  },
-]);
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route path="/movie/:id" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={App} />);
+root.render(<App />);

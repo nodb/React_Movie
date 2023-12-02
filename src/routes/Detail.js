68a9dff1a91ffc66e9552fe59638b34eca92a604
nodb <scnoh02@naver.com>
2023-12-02 17:13:13 +0900

    add: Movie

---
+++ src/routes/Detail.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Movie from "../components/Movie";
+
+function Detail() {
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovies(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <Movie
+            key={movies.id}
+            id={movies.id}
+            coverImg={movies.medium_cover_image}
+            title={movies.title}
+            summary={movies.description_full}
+            genres={movies.genres}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+export default Detail;
